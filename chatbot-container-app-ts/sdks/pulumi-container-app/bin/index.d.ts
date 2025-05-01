046d51ac93d5d20c27830d0272d2f9adf8cda2fb
Komal <komal@pulumi.com>
2025-05-01 10:05:38 -0700

    Delete node modules

--- chatbot-container-app-ts/sdks/pulumi-container-app/bin/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { ContainerAppArgs } from "./containerApp";
-export type ContainerApp = import("./containerApp").ContainerApp;
-export declare const ContainerApp: typeof import("./containerApp").ContainerApp;
-export { ProviderArgs } from "./provider";
-export type Provider = import("./provider").Provider;
-export declare const Provider: typeof import("./provider").Provider;
